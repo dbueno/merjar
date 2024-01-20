22fa0c1c0c6c3fb59a2ed4dd4c00b6ba2e079068
Denis Bueno <dbueno@gmail.com>
2024-01-20 10:28:47 -0700

    initial revision

---
+++ README.md
@@ -0,0 +1,1 @@
+Merge JAR files safely
---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flake-compat } :
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        merjar-pkg = { python3, makeWrapper, lib, ghidra }: python3.pkgs.buildPythonPackage rec {
+          name = "merjar";
+          version = "0.0.1";
+          src =
+            let
+              elide = [ "jadx-cli-1.4.5.jar" "jadx-core-1.4.5.jar" ];
+            in
+            with builtins;
+            filterSource
+            (path: type: !(elem (baseNameOf path) elide))
+            ./.;
+
+
+          doCheck = false;
+        };
+
+        merjar = pkgs.callPackage merjar-pkg {};
+
+      in {
+        packages = {
+          inherit merjar;
+        };
+        defaultPackage = merjar;
+        devShell =
+          pkgs.mkShell {
+            inputsFrom = [ ];
+            packages =
+              with pkgs; [
+                python3
+                pyright
+              ];
+            hardeningDisable = [ "all" ];
+            shellHook = ''
+              export PYTHONPATH=`pwd`/src
+            '';
+          };
+      });
+}
+
+
